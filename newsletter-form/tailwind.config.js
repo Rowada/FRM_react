fce4cff952e8aec74be232f2d735881959a3a1f7
Josue <96113041+Rowada@users.noreply.github.com>
2024-02-22 10:04:41 +0100

    feat: add tailwind theme

--- newsletter-form/tailwind.config.js
+++ newsletter-form/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "hsl(234, 29%, 20%)",
+        primary: "hsl(4, 100%, 67%)",
+        secondary: "hsl(0, 0%, 100%)",
+        accent: "hsl(235, 18%, 26%)",
+        background: "hsl(231, 7%, 60%)",
+      },
+      fontFamily: {
+        heading: ['"Roboto"', "sans-serif"],
+        body: ['"Roboto"', "sans-serif"],
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+    },
   },
   plugins: [],
 };
