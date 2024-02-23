4059e101ff0231eabbb6ecb66996ef5279c2b156
Josue <96113041+Rowada@users.noreply.github.com>
2024-02-23 13:20:40 +0100

    feat: add error message

--- newsletter-form/src/components/Form/Form.jsx
+++ newsletter-form/src/components/Form/Form.jsx
@@ -1,8 +1,35 @@
 // @ts-nocheck
 
-export default function Form() {
+import { useState } from "react";
+
+export default function Form({ onSubmit }) {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Valid email required");
+    } else {
+      setError("");
+      onSubmit(email);
+    }
+  };
+
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
   return (
-    <form className="relative flex flex-col justify-center my-6 md:mt-8 px-5 md:px-10">
+    <form
+      onSubmit={handleSubmit}
+      className="relative flex flex-col justify-center my-6 md:mt-8 px-5 md:px-10"
+    >
       <div className="flex flex-col items-start">
         <label className="font-bold text-xs mb-1" htmlFor="new">
           Email adress
@@ -11,15 +38,19 @@ export default function Form() {
           className="border border-gray-300 hover:border-primary focus:outline-none focus:border-primary text-accent w-full mb-4 rounded-md py-3 px-3"
           type="email"
           placeholder="email@company.com"
+          value={email}
           id="new"
+          onChange={handleChange}
         />
       </div>
       <button className="block bg-text text-secondary font-bold text-sm rounded-md py-3 px-3 hover:bg-gradient-to-l hover:from-primary hover:to-hover">
         Subscribe to monthly newsletter
       </button>
-      <span className="absolute top-0 right-5 md:right-10 font-bold text-xs text-primary">
-        Valid email required
-      </span>
+      {error && (
+        <span className="absolute top-0 right-5 md:right-10 font-bold text-xs text-primary">
+          {error}
+        </span>
+      )}
     </form>
   );
 }
---
+++ newsletter-form/src/components/Header/Success.jsx
@@ -0,0 +1,5 @@
+// @ts-nocheck
+
+export default function Success() {
+  return <div>Success</div>;
+}
