61ce4f154318bc0cdbf09bd1d06a3831d5603e12
Josue <96113041+Rowada@users.noreply.github.com>
2024-02-27 14:23:28 +0100

    style: add default main view

--- age-calculator/src/components/age/Age.jsx
+++ age-calculator/src/components/age/Age.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const Age = () => {
-  return <div>Age</div>;
+  return (
+    <div className="py-5">
+      <h2 className="text-7xl font-extrabolditalic italic">
+        <span className="text-primary">--</span>
+        years
+      </h2>
+      <h2 className="text-7xl font-extrabolditalic">
+        <span className="text-primary">--</span>
+        months
+      </h2>
+      <h2 className="text-7xl font-extrabolditalic">
+        <span className="text-primary">--</span>
+        days
+      </h2>
+    </div>
+  );
 };
