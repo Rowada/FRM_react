2608b94ca816ffdd1a4c7b54ebd34bf2b0101269
Josue <96113041+Rowada@users.noreply.github.com>
2024-02-27 14:03:32 +0100

    style: add the first version of the form

--- age-calculator/src/components/form/Form.jsx
+++ age-calculator/src/components/form/Form.jsx
@@ -1,5 +1,51 @@
 import React from "react";
+import arrowIcon from "../../assets/images/icon-arrow.svg";
 
 export const Form = () => {
-  return <div>Form</div>;
+  return (
+    <div>
+      <form>
+        <div className="flex items-center justify-start gap-4">
+          <div className="flex flex-col">
+            <label className="text-accent uppercase" htmlFor="day">
+              day
+            </label>
+            <input
+              className="border block border-background rounded-md py-3 pl-3"
+              type="text"
+              id="day"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-accent uppercase" htmlFor="month">
+              month
+            </label>
+            <input
+              className="border border-background rounded-md py-3 pl-3"
+              type="text"
+              id="month"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-accent uppercase" htmlFor="year">
+              year
+            </label>
+            <input
+              className="border border-background rounded-md py-3 pl-3"
+              type="text"
+              id="year"
+            />
+          </div>
+        </div>
+
+        <div className="flex mt-10 relative">
+          <span className="bg-background h-[1px] w-full"></span>
+
+          <button className="rounded-full p-3 bg-primary absolute -top-8 right-0 hover:bg-text">
+            <img className="w-10 h-auto" src={arrowIcon} alt="arrow-icon" />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
--- age-calculator/src/components/main/Main.jsx
+++ age-calculator/src/components/main/Main.jsx
@@ -5,7 +5,7 @@ import { Form } from "../form/Form";
 
 export const Main = () => {
   return (
-    <div>
+    <div className="bg-white rounded-xl rounded-br-4xl p-5 w-[800px]">
       <Form />
 
       <Age />
