79631d0261b91a466e8a9aa29473f5e4a38d52e2
Josue <96113041+Rowada@users.noreply.github.com>
2024-03-04 11:25:03 +0100

    build: add tailwind theme

--- advice-generator/src/App.jsx
+++ advice-generator/src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-extrabold underline">
+      It is easy to sit up and take notice, what's difficult is getting up and
+      taking action.
+    </h1>
+  );
 }
 
 export default App;
--- advice-generator/tailwind.config.js
+++ advice-generator/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "hsl(193, 38%, 86%)",
+        primary: "hsl(150, 100%, 66%)",
+        accent: "hsl(217, 19%, 38%)",
+        background: "hsl(217, 19%, 24%)",
+        darkblue: "hsl(218, 23%, 16%)",
+      },
+
+      fontFamily: {
+        body: ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
